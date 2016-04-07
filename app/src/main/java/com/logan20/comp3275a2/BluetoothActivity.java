6948f50e46d44f5c914f0689125638e66ddc996a
Christine <christinerussell18@live.com>
2016-04-07 17:45:30 -0400

    a

--- app/src/main/java/com/logan20/comp3275a2/BluetoothActivity.java
+++ app/src/main/java/com/logan20/comp3275a2/BluetoothActivity.java
@@ -1,14 +1,22 @@
 package com.logan20.comp3275a2;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class BluetoothActivity extends AppCompatActivity {
 
     private static final int REQUEST_ENABLE_BT = 1;
-
+    ArrayList<String> devices = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +29,21 @@ public class BluetoothActivity extends AppCompatActivity {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
+
+        final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    devices.add(device.getName() + "\n" + device.getAddress());
+                }
+            }
+        };
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,devices);
+        ListView lv = (ListView)findViewById(R.id.device_view);
+        lv.setAdapter(itemsAdapter);
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(broadcastReceiver, filter);
     }
 }
